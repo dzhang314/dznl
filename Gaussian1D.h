333e16124b1e7ad9b3a20b9cf7fa8dfd8061922d
David K. Zhang <dzhang314@gmail.com>
2017-01-16 10:48:43 -0600

    Templatized Gaussian1D

--- Gaussian1D.h
+++ Gaussian1D.h
@@ -7,56 +7,58 @@
 
 namespace dznl {
 
+    template<typename TReal>
     class Gaussian1D {
 
     private: // =================================================== DATA MEMBERS
 
-        double a; // inverse width
-        double c; // center
+        TReal a; // inverse width
+        TReal c; // center
 
     public: // ===================================================== CONSTRUCTOR
 
         Gaussian1D() : a(1.0), c(0.0) {}
 
-        Gaussian1D(double invWidth) : a(invWidth), c(0.0) {}
+        Gaussian1D(const TReal &invWidth) : a(invWidth), c() {}
 
-        Gaussian1D(double invWidth, double center) : a(invWidth), c(center) {}
+        Gaussian1D(const TReal &invWidth, const TReal &center) :
+                a(invWidth), c(center) {}
 
     public: // ====================================================== EVALUATION
 
-        double evaluateAt(double x) const {
-            const double dist = x - c;
-            return std::exp(-0.5 * a * dist * dist);
+        TReal evaluateAt(const TReal &x) const {
+            const TReal dist = x - c;
+            return exp(-0.5 * a * dist * dist);
         }
 
-        double operator()(double x) const {
+        TReal operator()(const TReal &x) const {
             return evaluateAt(x);
         }
 
     public: // ================================================= MATRIX ELEMENTS
 
-        static double innerProduct(const Gaussian1D &g1, const Gaussian1D &g2) {
-            const double aSum = g1.a + g2.a;
-            const double dist = g2.c - g1.c;
-            return std::sqrt(constants::tau / aSum) *
-                   std::exp(-0.5 * g1.a * g2.a * dist * dist / aSum);
+        static TReal innerProduct(const Gaussian1D &g1, const Gaussian1D &g2) {
+            const TReal aSum = g1.a + g2.a;
+            const TReal dist = g2.c - g1.c;
+            return sqrt(constants::tau / aSum) *
+                   exp(-0.5 * g1.a * g2.a * dist * dist / aSum);
         }
 
-        double operator*(const Gaussian1D &g) const {
+        TReal operator*(const Gaussian1D &g) const {
             return innerProduct(*this, g);
         }
 
-        static double xMatrixElement(const Gaussian1D &g1, const Gaussian1D &g2,
-                                     unsigned int n) {
-            const double aInv = 1.0 / sqrt(g1.a + g2.a);
+        static TReal xMatrixElement(const Gaussian1D &g1, const Gaussian1D &g2,
+                                    unsigned int n) {
+            const TReal aInv = 1.0 / sqrt(g1.a + g2.a);
             return innerProduct(g1, g2) * pow(aInv, n) *
                    hermiteHi(n, aInv * (g1.a * g1.c + g2.a * g2.c));
         }
 
-        static double ddxMatrixElement(const Gaussian1D &g1,
-                                       const Gaussian1D &g2,
-                                       unsigned int n) {
-            const double aInv = sqrt(g1.a * g2.a / (g1.a + g2.a));
+        static TReal ddxMatrixElement(const Gaussian1D &g1,
+                                      const Gaussian1D &g2,
+                                      unsigned int n) {
+            const TReal aInv = sqrt(g1.a * g2.a / (g1.a + g2.a));
             return innerProduct(g1, g2) * pow(aInv, n) *
                    hermiteHe(n, aInv * (g2.c - g1.c));
         }
