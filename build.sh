7767d8ae464bb8f2d213fad71c60e23a1732f372
David K. Zhang <dzhang314@gmail.com>
2023-06-29 02:49:49 -0700

    fix wasm link flags

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ set -e
 
 NATIVE_FLAGS=(
     -march=native
+    -c
 )
 
 WASM_FLAGS=(
@@ -9,6 +10,7 @@ WASM_FLAGS=(
     -mmultivalue
     -Xclang=-target-abi
     -Xclang=experimental-mv
+    -Wl,--no-entry
 )
 
 WARNING_FLAGS=(
@@ -18,11 +20,11 @@ WARNING_FLAGS=(
 )
 
 clang++ -std=c++20 ${NATIVE_FLAGS[*]} ${WARNING_FLAGS[*]} -O3 \
-    -nostdlib -Icpp -c native_interface.cpp -o dznl.o
+    -nostdlib -Icpp native_interface.cpp -o dznl.o
 
 objdump -M intel -d --no-show-raw-insn dznl.o
 
 clang++ -std=c++20 ${WASM_FLAGS[*]} ${WARNING_FLAGS[*]} -O3 \
-    -nostdlib -Icpp -c wasm_interface.cpp -o dznl.wasm
+    -nostdlib -Icpp wasm_interface.cpp -o dznl.wasm
 
 wasm2wat dznl.wasm
