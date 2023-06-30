4971a6f26d20794b929c92bd69d494b4848a4e93
David K. Zhang <dzhang314@gmail.com>
2023-06-30 02:19:38 -0700

    rename Random -> RandomNumbers

--- cpp/RandomNumbers.hpp
+++ cpp/RandomNumbers.hpp
@@ -1,9 +1,8 @@
-#ifndef DZNL_RANDOM_HPP_INCLUDED
-#define DZNL_RANDOM_HPP_INCLUDED
+#ifndef DZNL_RANDOM_NUMBERS_HPP
+#define DZNL_RANDOM_NUMBERS_HPP
 
-#include "Arithmetic.hpp" // for sqrt
-#include "Constants.hpp"  // for zero, one
-#include "Types.hpp"      // for f64, u64
+#include "MathematicalFunctions.hpp"
+#include "NumericTypes.hpp"
 
 namespace dznl {
 
@@ -11,10 +10,10 @@ namespace dznl {
 constexpr f64 random_advance(u64 &seed) noexcept {
     constexpr u64 multiplier = 6364136223846793005ULL;
     constexpr u64 increment = 1442695040888963407ULL;
-    constexpr f64 denominator = 18446744073709551616.0;
+    constexpr f64 denominator = 1.0 / 18446744073709551616.0;
     seed *= multiplier;
     seed += increment;
-    return static_cast<f64>(seed) / denominator;
+    return static_cast<f64>(seed) * denominator;
 }
 
 
@@ -27,34 +26,32 @@ void random_fill(T *__restrict__ result, I length, u64 seed) noexcept {
 
 
 template <int dimension, typename T, typename I>
-void random_fill_sphere(T *__restrict__ result, I num_points,
-                        u64 seed) noexcept {
+void random_fill_sphere(
+    T *__restrict__ result, I num_points, u64 seed
+) noexcept {
     for (; num_points > 0; --num_points) {
         T point[dimension];
         while (true) {
             f64 norm_squared = 0.0;
             for (int j = 0; j < dimension; ++j) {
-                const f64 random_value = 2.0 * random_advance(seed) - 1.0;
+                const f64 random_value =
+                    double_value(random_advance(seed)) - one<f64>();
                 point[j] = static_cast<T>(random_value);
-                norm_squared += random_value * random_value;
+                norm_squared += square(random_value);
             }
-            if (norm_squared < 1.0) {
-                break;
-            }
+            if (norm_squared < 1.0) { break; }
         }
         T norm = zero<T>();
         for (int j = 0; j < dimension; ++j) {
             const T x = point[j];
-            norm += x * x;
+            norm += square(x);
         }
         norm = sqrt(norm);
-        for (int j = 0; j < dimension; ++j) {
-            *result++ = point[j] / norm;
-        }
+        for (int j = 0; j < dimension; ++j) { *result++ = point[j] / norm; }
     }
 }
 
 
 } // namespace dznl
 
-#endif // DZNL_RANDOM_HPP_INCLUDED
+#endif // DZNL_RANDOM_NUMBERS_HPP
