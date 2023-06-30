32fae393ca19899e9530a494bb668731efca09b1
David K. Zhang <dzhang314@gmail.com>
2023-06-30 16:19:44 -0700

    Update wasm_interface.cpp

--- wasm_interface.cpp
+++ wasm_interface.cpp
@@ -1,148 +1,111 @@
+#include <dznl/ErrorFreeTransformations.hpp>
 #include <dznl/FloatingPointProperties.hpp>
 #include <dznl/MathematicalFunctions.hpp>
+#include <dznl/NumericTypes.hpp>
+#include <dznl/RandomNumbers.hpp>
 
 
-__attribute__((export_name("min_f32"))) dznl::f32 min_f32(dznl::f32, dznl::f32);
-__attribute__((export_name("min_f64"))) dznl::f64 min_f64(dznl::f64, dznl::f64);
-dznl::f32 min_f32(dznl::f32 x, dznl::f32 y) { return dznl::min(x, y); }
-dznl::f64 min_f64(dznl::f64 x, dznl::f64 y) { return dznl::min(x, y); }
+#define DZNL_WASM_EXPORT(NAME) __attribute__((export_name(NAME)))
 
 
-__attribute__((export_name("max_f32"))) dznl::f32 max_f32(dznl::f32, dznl::f32);
-__attribute__((export_name("max_f64"))) dznl::f64 max_f64(dznl::f64, dznl::f64);
-dznl::f32 max_f32(dznl::f32 x, dznl::f32 y) { return dznl::max(x, y); }
-dznl::f64 max_f64(dznl::f64 x, dznl::f64 y) { return dznl::max(x, y); }
+using dznl::u64;
+using dznl::f32;
+using dznl::f64;
 
 
-__attribute__((export_name("round_f32"))) dznl::f32 round_f32(dznl::f32);
-__attribute__((export_name("round_f64"))) dznl::f64 round_f64(dznl::f64);
-dznl::f32 round_f32(dznl::f32 x) { return dznl::round(x); }
-dznl::f64 round_f64(dznl::f64 x) { return dznl::round(x); }
+DZNL_WASM_EXPORT("min_f32") f32 min_f32(f32, f32);
+DZNL_WASM_EXPORT("min_f64") f64 min_f64(f64, f64);
+DZNL_WASM_EXPORT("max_f32") f32 max_f32(f32, f32);
+DZNL_WASM_EXPORT("max_f64") f64 max_f64(f64, f64);
+DZNL_WASM_EXPORT("round_f32") f32 round_f32(f32);
+DZNL_WASM_EXPORT("round_f64") f64 round_f64(f64);
+DZNL_WASM_EXPORT("ceil_f32") f32 ceil_f32(f32);
+DZNL_WASM_EXPORT("ceil_f64") f64 ceil_f64(f64);
+DZNL_WASM_EXPORT("floor_f32") f32 floor_f32(f32);
+DZNL_WASM_EXPORT("floor_f64") f64 floor_f64(f64);
+DZNL_WASM_EXPORT("trunc_f32") f32 trunc_f32(f32);
+DZNL_WASM_EXPORT("trunc_f64") f64 trunc_f64(f64);
+DZNL_WASM_EXPORT("abs_f32") f32 abs_f32(f32);
+DZNL_WASM_EXPORT("abs_f64") f64 abs_f64(f64);
+DZNL_WASM_EXPORT("sqrt_f32") f32 sqrt_f32(f32);
+DZNL_WASM_EXPORT("sqrt_f64") f64 sqrt_f64(f64);
+DZNL_WASM_EXPORT("copysign_f32") f32 copysign_f32(f32, f32);
+DZNL_WASM_EXPORT("copysign_f64") f64 copysign_f64(f64, f64);
 
 
-__attribute__((export_name("ceil_f32"))) dznl::f32 ceil_f32(dznl::f32);
-__attribute__((export_name("ceil_f64"))) dznl::f64 ceil_f64(dznl::f64);
-dznl::f32 ceil_f32(dznl::f32 x) { return dznl::ceil(x); }
-dznl::f64 ceil_f64(dznl::f64 x) { return dznl::ceil(x); }
+f32 min_f32(f32 x, f32 y) { return dznl::min(x, y); }
+f64 min_f64(f64 x, f64 y) { return dznl::min(x, y); }
+f32 max_f32(f32 x, f32 y) { return dznl::max(x, y); }
+f64 max_f64(f64 x, f64 y) { return dznl::max(x, y); }
+f32 round_f32(f32 x) { return dznl::round(x); }
+f64 round_f64(f64 x) { return dznl::round(x); }
+f32 ceil_f32(f32 x) { return dznl::ceil(x); }
+f64 ceil_f64(f64 x) { return dznl::ceil(x); }
+f32 floor_f32(f32 x) { return dznl::floor(x); }
+f64 floor_f64(f64 x) { return dznl::floor(x); }
+f32 trunc_f32(f32 x) { return dznl::trunc(x); }
+f64 trunc_f64(f64 x) { return dznl::trunc(x); }
+f32 abs_f32(f32 x) { return dznl::abs(x); }
+f64 abs_f64(f64 x) { return dznl::abs(x); }
+f32 sqrt_f32(f32 x) { return dznl::sqrt(x); }
+f64 sqrt_f64(f64 x) { return dznl::sqrt(x); }
+f32 copysign_f32(f32 x, f32 y) { return dznl::copysign(x, y); }
+f64 copysign_f64(f64 x, f64 y) { return dznl::copysign(x, y); }
 
 
-__attribute__((export_name("floor_f32"))) dznl::f32 floor_f32(dznl::f32);
-__attribute__((export_name("floor_f64"))) dznl::f64 floor_f64(dznl::f64);
-dznl::f32 floor_f32(dznl::f32 x) { return dznl::floor(x); }
-dznl::f64 floor_f64(dznl::f64 x) { return dznl::floor(x); }
+DZNL_WASM_EXPORT("compute_radix_f32") f32 compute_radix_f32();
+DZNL_WASM_EXPORT("compute_radix_f64") f64 compute_radix_f64();
+DZNL_WASM_EXPORT("compute_precision_f32") u64 compute_precision_f32();
+DZNL_WASM_EXPORT("compute_precision_f64") u64 compute_precision_f64();
 
 
-__attribute__((export_name("trunc_f32"))) dznl::f32 trunc_f32(dznl::f32);
-__attribute__((export_name("trunc_f64"))) dznl::f64 trunc_f64(dznl::f64);
-dznl::f32 trunc_f32(dznl::f32 x) { return dznl::trunc(x); }
-dznl::f64 trunc_f64(dznl::f64 x) { return dznl::trunc(x); }
+f32 compute_radix_f32() { return dznl::compute_radix<f32, u64>(); }
+f64 compute_radix_f64() { return dznl::compute_radix<f64, u64>(); }
+u64 compute_precision_f32() { return dznl::compute_precision<f32, u64>(); }
+u64 compute_precision_f64() { return dznl::compute_precision<f64, u64>(); }
 
 
-__attribute__((export_name("abs_f32"))) dznl::f32 abs_f32(dznl::f32);
-__attribute__((export_name("abs_f64"))) dznl::f64 abs_f64(dznl::f64);
-dznl::f32 abs_f32(dznl::f32 x) { return dznl::abs(x); }
-dznl::f64 abs_f64(dznl::f64 x) { return dznl::abs(x); }
+DZNL_WASM_EXPORT("fast_two_sum_f32") dznl::pair<f32, f32> fast_two_sum_f32(f32, f32);
+DZNL_WASM_EXPORT("fast_two_sum_f64") dznl::pair<f64, f64> fast_two_sum_f64(f64, f64);
+DZNL_WASM_EXPORT("two_sum_f32") dznl::pair<f32, f32> two_sum_f32(f32, f32);
+DZNL_WASM_EXPORT("two_sum_f64") dznl::pair<f64, f64> two_sum_f64(f64, f64);
+DZNL_WASM_EXPORT("two_prod_f32") dznl::pair<f32, f32> two_prod_f32(f32, f32);
+DZNL_WASM_EXPORT("two_prod_f64") dznl::pair<f64, f64> two_prod_f64(f64, f64);
 
 
-__attribute__((export_name("sqrt_f32"))) dznl::f32 sqrt_f32(dznl::f32);
-__attribute__((export_name("sqrt_f64"))) dznl::f64 sqrt_f64(dznl::f64);
-dznl::f32 sqrt_f32(dznl::f32 x) { return dznl::sqrt(x); }
-dznl::f64 sqrt_f64(dznl::f64 x) { return dznl::sqrt(x); }
+dznl::pair<f32, f32> fast_two_sum_f32(f32 a, f32 b) { return dznl::fast_two_sum(a, b); }
+dznl::pair<f64, f64> fast_two_sum_f64(f64 a, f64 b) { return dznl::fast_two_sum(a, b); }
+dznl::pair<f32, f32> two_sum_f32(f32 a, f32 b) { return dznl::two_sum(a, b); }
+dznl::pair<f64, f64> two_sum_f64(f64 a, f64 b) { return dznl::two_sum(a, b); }
+dznl::pair<f32, f32> two_prod_f32(f32 a, f32 b) { return dznl::two_prod(a, b); }
+dznl::pair<f64, f64> two_prod_f64(f64 a, f64 b) { return dznl::two_prod(a, b); }
 
 
-__attribute__((export_name("copysign_f32")))
-dznl::f32 copysign_f32(dznl::f32, dznl::f32);
-__attribute__((export_name("copysign_f64")))
-dznl::f64 copysign_f64(dznl::f64, dznl::f64);
-dznl::f32 copysign_f32(dznl::f32 x, dznl::f32 y) {
-    return dznl::copysign(x, y);
-}
-dznl::f64 copysign_f64(dznl::f64 x, dznl::f64 y) {
-    return dznl::copysign(x, y);
-}
-
-
-__attribute__((export_name("compute_radix_f32"))) dznl::f32 compute_radix_f32();
-__attribute__((export_name("compute_radix_f64"))) dznl::f64 compute_radix_f64();
-dznl::f32 compute_radix_f32() { return dznl::compute_radix<dznl::f32, int>(); }
-dznl::f64 compute_radix_f64() { return dznl::compute_radix<dznl::f64, int>(); }
-
-
-__attribute__((export_name("compute_precision_f32"))) dznl::f32
-compute_precision_f32();
-__attribute__((export_name("compute_precision_f64"))) dznl::f64
-compute_precision_f64();
-dznl::f32 compute_precision_f32() {
-    return dznl::compute_precision<dznl::f32, int>();
-}
-dznl::f64 compute_precision_f64() {
-    return dznl::compute_precision<dznl::f64, int>();
-}
-
-
-// __attribute__((export_name("fast_two_sum_f32")))
-// dznl::Pair<dznl::f32, dznl::f32>
-// fast_two_sum_f32(dznl::f32 a, dznl::f32 b) {
-//     return dznl::fast_two_sum(a, b);
-// }
-
-
-// __attribute__((export_name("fast_two_sum_f64")))
-// dznl::Pair<dznl::f64, dznl::f64>
-// fast_two_sum_f64(dznl::f64 a, dznl::f64 b) {
-//     return dznl::fast_two_sum(a, b);
-// }
-
-
-// __attribute__((export_name("two_sum_f32"))) dznl::Pair<dznl::f32, dznl::f32>
-// two_sum_f32(dznl::f32 a, dznl::f32 b) {
-//     return dznl::two_sum(a, b);
-// }
-
-
-// __attribute__((export_name("two_sum_f64"))) dznl::Pair<dznl::f64, dznl::f64>
-// two_sum_f64(dznl::f64 a, dznl::f64 b) {
-//     return dznl::two_sum(a, b);
-// }
-
-
-// __attribute__((export_name("two_prod_f32"))) dznl::Pair<dznl::f32, dznl::f32>
-// two_prod_f32(dznl::f32 a, dznl::f32 b) {
-//     return dznl::two_prod(a, b);
-// }
-
-
-// __attribute__((export_name("two_prod_f64"))) dznl::Pair<dznl::f64, dznl::f64>
-// two_prod_f64(dznl::f64 a, dznl::f64 b) {
-//     return dznl::two_prod(a, b);
-// }
-
-
-// __attribute__((export_name("random_fill_f32"))) void random_fill_f32(
-//     dznl::f32 *__restrict__ result, dznl::u32 length, dznl::u64 seed
+// DZNL_WASM_EXPORT("random_fill_f32") void random_fill_f32(
+//     f32 *__restrict__ result, dznl::u32 length, dznl::u64 seed
 // ) {
 //     dznl::random_fill(result, length, seed);
 // }
 
 
-// __attribute__((export_name("random_fill_f64"))) void random_fill_f64(
-//     dznl::f64 *__restrict__ result, dznl::u32 length, dznl::u64 seed
+// DZNL_WASM_EXPORT("random_fill_f64") void random_fill_f64(
+//     f64 *__restrict__ result, dznl::u32 length, dznl::u64 seed
 // ) {
 //     dznl::random_fill(result, length, seed);
 // }
 
 
-// __attribute__((export_name("random_fill_sphere_f32"))) void
+// DZNL_WASM_EXPORT("random_fill_sphere_f32") void
 // random_fill_sphere_f32(
-//     dznl::f32 *__restrict__ result, dznl::u32 num_points, dznl::u64 seed
+//     f32 *__restrict__ result, dznl::u32 num_points, dznl::u64 seed
 // ) {
 //     dznl::random_fill_sphere<3>(result, num_points, seed);
 // }
 
 
-// __attribute__((export_name("random_fill_sphere_f64"))) void
+// DZNL_WASM_EXPORT("random_fill_sphere_f64") void
 // random_fill_sphere_f64(
-//     dznl::f64 *__restrict__ result, dznl::u32 num_points, dznl::u64 seed
+//     f64 *__restrict__ result, dznl::u32 num_points, dznl::u64 seed
 // ) {
 //     dznl::random_fill_sphere<3>(result, num_points, seed);
 // }
