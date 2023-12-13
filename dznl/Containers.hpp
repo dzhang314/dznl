051991b76633b96e7ab011c5fb36709c0fa03e29
David K. Zhang <dzhang314@gmail.com>
2023-12-12 23:41:08 -0800

    add more tuple-like containers

--- dznl/Containers.hpp
+++ dznl/Containers.hpp
@@ -4,10 +4,37 @@
 namespace dznl {
 
 
-template <typename T, typename U>
+template <typename T1, typename T2>
 struct Pair {
-    T first;
-    U second;
+    T1 first;
+    T2 second;
+};
+
+
+template <typename T1, typename T2, typename T3>
+struct Triple {
+    T1 first;
+    T2 second;
+    T3 third;
+};
+
+
+template <typename T1, typename T2, typename T3, typename T4>
+struct Quadruple {
+    T1 first;
+    T2 second;
+    T3 third;
+    T4 fourth;
+};
+
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+struct Quintuple {
+    T1 first;
+    T2 second;
+    T3 third;
+    T4 fourth;
+    T5 fifth;
 };
 
 
