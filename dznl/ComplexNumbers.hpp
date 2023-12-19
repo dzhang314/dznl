5e4149056e8120e1b733e7d92a6477c788a53acd
David K. Zhang <dzhang314@gmail.com>
2023-12-19 02:49:06 -0600

    implement zero/one/is_zero/is_one for ComplexNumber and DualNumber

--- dznl/ComplexNumbers.hpp
+++ dznl/ComplexNumbers.hpp
@@ -1,6 +1,8 @@
 #ifndef DZNL_COMPLEX_NUMBERS_HPP_INCLUDED
 #define DZNL_COMPLEX_NUMBERS_HPP_INCLUDED
 
+#include "NumericTypeInterface.hpp"
+
 namespace dznl {
 
 
@@ -54,6 +56,30 @@ operator*(const ComplexNumber<T> &lhs, const ComplexNumber<T> &rhs) noexcept {
         lhs.real * rhs.real - lhs.imag * rhs.imag,
         lhs.real * rhs.imag + lhs.imag * rhs.real
     };
+}
+
+
+template <typename T>
+constexpr ComplexNumber<T> zero<ComplexNumber<T>>() noexcept {
+    return {zero<T>(), zero<T>()};
+}
+
+
+template <typename T>
+constexpr ComplexNumber<T> one<ComplexNumber<T>>() noexcept {
+    return {one<T>(), zero<T>()};
+}
+
+
+template <typename T>
+constexpr bool is_zero<ComplexNumber<T>>(const ComplexNumber<T> &x) noexcept {
+    return is_zero(x.real) && is_zero(x.dual);
+}
+
+
+template <typename T>
+constexpr bool is_one<ComplexNumber<T>>(const ComplexNumber<T> &x) noexcept {
+    return is_one(x.real) && is_zero(x.dual);
 }
 
 
--- dznl/DualNumbers.hpp
+++ dznl/DualNumbers.hpp
@@ -1,6 +1,8 @@
 #ifndef DZNL_DUAL_NUMBERS_HPP_INCLUDED
 #define DZNL_DUAL_NUMBERS_HPP_INCLUDED
 
+#include "NumericTypeInterface.hpp"
+
 namespace dznl {
 
 
@@ -51,6 +53,30 @@ template <typename T>
 constexpr DualNumber<T>
 operator*(const DualNumber<T> &lhs, const DualNumber<T> &rhs) noexcept {
     return {lhs.real * rhs.real, lhs.real * rhs.dual + lhs.dual * rhs.real};
+}
+
+
+template <typename T>
+constexpr DualNumber<T> zero<DualNumber<T>>() noexcept {
+    return {zero<T>(), zero<T>()};
+}
+
+
+template <typename T>
+constexpr DualNumber<T> one<DualNumber<T>>() noexcept {
+    return {one<T>(), zero<T>()};
+}
+
+
+template <typename T>
+constexpr bool is_zero<DualNumber<T>>(const DualNumber<T> &x) noexcept {
+    return is_zero(x.real) && is_zero(x.dual);
+}
+
+
+template <typename T>
+constexpr bool is_one<DualNumber<T>>(const DualNumber<T> &x) noexcept {
+    return is_one(x.real) && is_zero(x.dual);
 }
 
 
