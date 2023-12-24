55f11972e59a4c1a62cb3a04da116a3c10ed580e
David K. Zhang <dzhang314@gmail.com>
2023-12-24 03:21:37 -0600

    add new RandomNumberGenerator

---
+++ dznl/RandomNumberGenerator.hpp
@@ -0,0 +1,77 @@
+#ifndef DZNL_RANDOM_NUMBER_GENERATOR_HPP
+#define DZNL_RANDOM_NUMBER_GENERATOR_HPP
+
+#include "NumericFunctions.hpp"
+#include "NumericTypes.hpp"
+
+namespace dznl {
+
+
+template <typename ITEM_T, typename INDEX_T, INDEX_T SIZE>
+struct Array {
+
+    ITEM_T items[SIZE];
+
+    ITEM_T &operator[](INDEX_T index) noexcept { return items[index]; }
+
+    const ITEM_T &operator[](INDEX_T index) const noexcept {
+        return items[index];
+    }
+
+}; // struct Array<ITEM_T, INDEX_T, SIZE>
+
+
+class RandomNumberGenerator {
+
+    static constexpr u64 MULTIPLIER = 6364136223846793005ULL;
+    static constexpr u64 INCREMENT = 1442695040888963407ULL;
+    static constexpr f64 DENOMINATOR = 1.0 / 18446744073709551616.0;
+
+    u64 state;
+
+public:
+
+    explicit constexpr RandomNumberGenerator(u64 seed) noexcept
+        : state(seed) {}
+
+    constexpr u64 random_integer() noexcept {
+        state *= MULTIPLIER;
+        state += INCREMENT;
+        return state;
+    }
+
+    constexpr f64 random_real() noexcept {
+        state *= MULTIPLIER;
+        state += INCREMENT;
+        return static_cast<f64>(state) * DENOMINATOR;
+    }
+
+    template <typename REAL_T, typename INDEX_T, INDEX_T DIMENSION>
+    constexpr Array<REAL_T, INDEX_T, DIMENSION> random_sphere_point() noexcept {
+        Array<REAL_T, INDEX_T, DIMENSION> result;
+        while (true) {
+            f64 norm_squared = zero<f64>();
+            for (INDEX_T i = zero<INDEX_T>(); i < DIMENSION; ++i) {
+                const f64 x = twice(random_real()) - one<f64>();
+                result[i] = static_cast<REAL_T>(x);
+                norm_squared += square(x);
+            }
+            if (norm_squared <= one<f64>()) { break; }
+        }
+        REAL_T norm = zero<REAL_T>();
+        for (INDEX_T i = zero<INDEX_T>(); i < DIMENSION; ++i) {
+            norm += square(result[i]);
+        }
+        norm = sqrt(norm);
+        for (INDEX_T i = zero<INDEX_T>(); i < DIMENSION; ++i) {
+            result[i] /= norm;
+        }
+        return result;
+    }
+
+}; // class RandomNumberGenerator
+
+
+} // namespace dznl
+
+#endif // DZNL_RANDOM_NUMBER_GENERATOR_HPP
