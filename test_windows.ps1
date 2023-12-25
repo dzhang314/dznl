128f831a09488f37a9a325345dedeb2bb43d6ee0
David K. Zhang <dzhang314@gmail.com>
2023-12-25 03:52:21 -0600

    add windows test run script

---
+++ test_windows.ps1
@@ -0,0 +1,22 @@
+Remove-Item -Path "bin" -Recurse -Force
+New-Item -Path "bin" -ItemType "directory" -Force
+
+$ClangWarningFlags = @(
+    "-Weverything"
+    "-Wno-c++98-compat"
+    "-Wno-c++98-compat-pedantic"
+    "-Wno-float-equal"
+    "-Wno-padded"
+)
+
+g++ -std=c++17 -Wall -Wextra -Werror -pedantic -pedantic-errors `
+    -I. test\TestFloatingPointProperties.cpp -lCatch2Main -lCatch2 `
+    -o bin\TestFloatingPointPropertiesGCC
+
+.\bin\TestFloatingPointPropertiesGCC
+
+clang++ -std=c++17 $ClangWarningFlags `
+    -I. test\TestFloatingPointProperties.cpp -lCatch2Main -lCatch2 `
+    -o bin\TestFloatingPointPropertiesClang
+
+.\bin\TestFloatingPointPropertiesClang
