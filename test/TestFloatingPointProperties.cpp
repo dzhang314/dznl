9111c7bcbda34ae8b950ecb54540375a42ee2504
David K. Zhang <dzhang314@gmail.com>
2024-01-05 17:53:59 -0600

    revise TestFloatingPointProperties to use constexpr context

--- test/TestFloatingPointProperties.cpp
+++ test/TestFloatingPointProperties.cpp
@@ -11,16 +11,25 @@
 
 
 TEST_CASE("compute_float_radix (IEEE binary)") {
-    CHECK_EQ(dznl::compute_float_radix<float, int>().second, FLT_RADIX);
-    CHECK_EQ(dznl::compute_float_radix<double, int>().second, FLT_RADIX);
-    CHECK_EQ(dznl::compute_float_radix<long double, int>().second, FLT_RADIX);
+    constexpr auto float_radix = dznl::compute_float_radix<float, int>();
+    CHECK_EQ(float_radix.second, FLT_RADIX);
+    constexpr auto double_radix = dznl::compute_float_radix<double, int>();
+    CHECK_EQ(double_radix.second, FLT_RADIX);
+    constexpr auto long_double_radix =
+        dznl::compute_float_radix<long double, int>();
+    CHECK_EQ(long_double_radix.second, FLT_RADIX);
 }
 
 
 TEST_CASE("compute_float_precision (IEEE binary)") {
-    CHECK_EQ(dznl::compute_float_precision<float, int>(), FLT_MANT_DIG);
-    CHECK_EQ(dznl::compute_float_precision<double, int>(), DBL_MANT_DIG);
-    CHECK_EQ(dznl::compute_float_precision<long double, int>(), LDBL_MANT_DIG);
+    constexpr int float_precision = dznl::compute_float_precision<float, int>();
+    CHECK_EQ(float_precision, FLT_MANT_DIG);
+    constexpr int double_precision =
+        dznl::compute_float_precision<double, int>();
+    CHECK_EQ(double_precision, DBL_MANT_DIG);
+    constexpr int long_double_precision =
+        dznl::compute_float_precision<long double, int>();
+    CHECK_EQ(long_double_precision, LDBL_MANT_DIG);
 }
 
 
@@ -42,15 +51,24 @@ template <> decimal128 one<decimal128>() noexcept { return 1; }
 } // clang-format on
 
 TEST_CASE("compute_float_radix (IEEE decimal)") {
-    CHECK_EQ(dznl::compute_float_radix<decimal32, int>().second, 10);
-    CHECK_EQ(dznl::compute_float_radix<decimal64, int>().second, 10);
-    CHECK_EQ(dznl::compute_float_radix<decimal128, int>().second, 10);
+    const auto decimal32_radix = dznl::compute_float_radix<decimal32, int>();
+    CHECK_EQ(decimal32_radix.second, 10);
+    const auto decimal64_radix = dznl::compute_float_radix<decimal64, int>();
+    CHECK_EQ(decimal64_radix.second, 10);
+    const auto decimal128_radix = dznl::compute_float_radix<decimal128, int>();
+    CHECK_EQ(decimal128_radix.second, 10);
 }
 
 TEST_CASE("compute_float_precision (IEEE decimal)") {
-    CHECK_EQ(dznl::compute_float_precision<decimal32, int>(), 7);
-    CHECK_EQ(dznl::compute_float_precision<decimal64, int>(), 16);
-    CHECK_EQ(dznl::compute_float_precision<decimal128, int>(), 34);
+    const auto decimal32_precision =
+        dznl::compute_float_precision<decimal32, int>();
+    CHECK_EQ(decimal32_precision, 7);
+    const auto decimal64_precision =
+        dznl::compute_float_precision<decimal64, int>();
+    CHECK_EQ(decimal64_precision, 16);
+    const auto decimal128_precision =
+        dznl::compute_float_precision<decimal128, int>();
+    CHECK_EQ(decimal128_precision, 34);
 }
 
 #endif
