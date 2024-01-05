b8b49af96e8d261dce5193473edc9c6cdb7875d3
David K. Zhang <dzhang314@gmail.com>
2024-01-05 17:25:59 -0600

    Update RieszEnergy.hpp

--- dznl/RieszEnergy.hpp
+++ dznl/RieszEnergy.hpp
@@ -7,22 +7,30 @@
 namespace dznl {
 
 
-template <typename REAL_T, typename INDEX_T>
+template <
+    typename REAL_T,
+    typename INDEX_T,
+    typename ACCESSOR_T = DZNL_CONST REAL_T *DZNL_RESTRICT>
 constexpr REAL_T riesz_energy(
-    const REAL_T *const DZNL_RESTRICT points,
-    const INDEX_T num_points,
-    const INDEX_T dimension
+    DZNL_CONST ACCESSOR_T &points,
+    DZNL_CONST INDEX_T &num_points,
+    DZNL_CONST INDEX_T &dimension
 ) noexcept {
     REAL_T result = zero<REAL_T>();
-    for (INDEX_T j = one<INDEX_T>(); j < num_points; ++j) {
+    for (INDEX_T j = zero<INDEX_T>(); j < num_points; ++j) {
+        DZNL_CONST INDEX_T offset_j = j * dimension;
+        DZNL_CONST ACCESSOR_T point_j = points + offset_j;
         for (INDEX_T i = zero<INDEX_T>(); i < j; ++i) {
+            DZNL_CONST INDEX_T offset_i = i * dimension;
+            DZNL_CONST ACCESSOR_T point_i = points + offset_i;
             REAL_T dist_sq = zero<REAL_T>();
             for (INDEX_T k = zero<INDEX_T>(); k < dimension; ++k) {
-                dist_sq += square(
-                    points[i * dimension + k] - points[j * dimension + k]
-                );
+                DZNL_CONST REAL_T diff = point_i[k] - point_j[k];
+                dist_sq += square(diff);
             }
-            result += inv(sqrt(dist_sq));
+            DZNL_CONST REAL_T dist = sqrt(dist_sq);
+            DZNL_CONST REAL_T inv_dist = inv(dist);
+            result += inv_dist;
         }
     }
     return result;
