c1bce2c7a86c4f07f4b97a3a8487b421cbb0d144
David K. Zhang <dzhang314@gmail.com>
2024-12-08 16:38:37 -0800

    previous commit continued

--- include/NumericTypes.hpp
+++ include/NumericTypes.hpp
@@ -28,12 +28,38 @@ static_assert(sizeof(f32) == 4);
 static_assert(sizeof(f64) == 8);
 
 
-#ifdef __SIZEOF_INT128__
+#ifdef DZNL_REQUEST_I128
 using i128 = __int128_t;
+#endif // DZNL_REQUEST_I128
+
+
+#ifdef DZNL_REQUEST_U128
 using u128 = __uint128_t;
-static_assert(sizeof(i128) == 16);
-static_assert(sizeof(u128) == 16);
-#endif // __SIZEOF_INT128__
+#endif // DZNL_REQUEST_U128
+
+
+#ifdef DZNL_REQUEST_D32
+#if defined(__clang__) || !defined(__GNUC__)
+#error "decimal floating-point support requires GCC"
+#endif // defined(__clang__) || !defined(__GNUC__)
+using d32 = float [[gnu::mode(SD)]];
+#endif // DZNL_REQUEST_D32
+
+
+#ifdef DZNL_REQUEST_D64
+#if defined(__clang__) || !defined(__GNUC__)
+#error "decimal floating-point support requires GCC"
+#endif // defined(__clang__) || !defined(__GNUC__)
+using d64 = float [[gnu::mode(DD)]];
+#endif // DZNL_REQUEST_D64
+
+
+#ifdef DZNL_REQUEST_D128
+#if defined(__clang__) || !defined(__GNUC__)
+#error "decimal floating-point support requires GCC"
+#endif // defined(__clang__) || !defined(__GNUC__)
+using d128 = float [[gnu::mode(TD)]];
+#endif // DZNL_REQUEST_D128
 
 
 } // namespace dznl
