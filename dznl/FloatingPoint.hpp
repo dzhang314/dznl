2265c298684813dc56c1742376345fb67414874e
David K. Zhang <dzhang314@gmail.com>
2024-12-11 03:33:44 -0800

    revise FloatingPoint.hpp

--- dznl/FloatingPoint.hpp
+++ dznl/FloatingPoint.hpp
@@ -2,65 +2,64 @@
 #define DZNL_FLOATING_POINT_HPP_INCLUDED
 
 #include "NumericConstants.hpp"
+#include "Tuple.hpp"
 
 namespace dznl {
 
 
-template <typename FLOAT_T>
-constexpr int compute_radix() {
+template <typename FLOAT_T, typename INTEGER_T = int>
+constexpr Tuple<FLOAT_T, INTEGER_T> compute_radix() {
 
     // The radix of a floating-point type is the smallest value of ulp(x) that
     // exceeds 1. For example, a binary floating-point type has possible ulp
     // values ..., 0.125, 0.25, 0.5, 1, **2**, 4, 8, ...; the radix is 2.
     // Similarly, a decimal floating-point type has possible ulp values
     // ..., 0.001, 0.01, 0.1, 1, **10**, 100, 1000, ...; the radix is 10.
-    const FLOAT_T ONE = one<FLOAT_T>();
+    const FLOAT_T FLOAT_ONE = one<FLOAT_T>();
 
     // Step 1: Find a minimal floating-point number x such that ulp(x) > 1.
     // It suffices to consider powers of two, because for any radix r >= 2,
     // a power of two always lies between r^k and r^(k + 1) for any power k.
-    FLOAT_T x = ONE;
+    FLOAT_T x = FLOAT_ONE;
     while (true) {
-        const FLOAT_T y = x + ONE;
+        const FLOAT_T y = x + FLOAT_ONE;
         const FLOAT_T z = y - x;
-        if (!(z == ONE)) { break; }
+        if (!(z == FLOAT_ONE)) { break; }
         x += x;
     }
 
     // Step 2: Determine ulp(x). This is the radix.
-    FLOAT_T radix = ONE;
-    int result = 1;
+    FLOAT_T float_radix = FLOAT_ONE;
+    INTEGER_T integer_radix = one<INTEGER_T>();
     while (true) {
-        const FLOAT_T y = x + radix;
+        const FLOAT_T y = x + float_radix;
         const FLOAT_T z = y - x;
-        if (z == radix) { break; }
-        radix += ONE;
-        ++result;
+        if (z == float_radix) { break; }
+        float_radix += FLOAT_ONE;
+        ++integer_radix;
     }
-    return result;
+    return {float_radix, integer_radix};
 }
 
 
-template <typename FLOAT_T>
+template <typename FLOAT_T, typename INTEGER_T = int>
 constexpr int compute_precision() {
 
     // Step 1: Compute the radix r as a floating-point number.
-    const FLOAT_T ONE = one<FLOAT_T>();
-    const int radix = compute_radix<FLOAT_T>();
-    FLOAT_T float_radix = ONE;
-    for (int i = 1; i < radix; ++i) { float_radix += ONE; }
+    const FLOAT_T float_radix = compute_radix<FLOAT_T, INTEGER_T>().first;
 
     // Step 2: Find the first power k such that ulp(r^k) > 1.
-    FLOAT_T power = ONE;
-    int result = 0;
+    const FLOAT_T FLOAT_ONE = one<FLOAT_T>();
+    FLOAT_T power = FLOAT_ONE;
+    INTEGER_T precision = 0;
     while (true) {
-        const FLOAT_T y = power + ONE;
+        const FLOAT_T y = power + FLOAT_ONE;
         const FLOAT_T z = y - power;
-        if (!(z == ONE)) { break; }
+        if (!(z == FLOAT_ONE)) { break; }
         power *= float_radix;
-        ++result;
+        ++precision;
     }
-    return result;
+    return precision;
 }
 
 
--- test/TestFloatingPoint.cpp
+++ test/TestFloatingPoint.cpp
@@ -11,42 +11,42 @@
 
 int main() {
 
-    static_assert(dznl::compute_radix<dznl::f16>() == 2);
+    static_assert(dznl::compute_radix<dznl::f16>().second == 2);
     static_assert(dznl::compute_precision<dznl::f16>() == 11);
-    static_assert(dznl::compute_radix<dznl::bf16>() == 2);
+    static_assert(dznl::compute_radix<dznl::bf16>().second == 2);
     static_assert(dznl::compute_precision<dznl::bf16>() == 8);
-    static_assert(dznl::compute_radix<dznl::f32>() == 2);
+    static_assert(dznl::compute_radix<dznl::f32>().second == 2);
     static_assert(dznl::compute_precision<dznl::f32>() == 24);
-    static_assert(dznl::compute_radix<dznl::f64>() == 2);
+    static_assert(dznl::compute_radix<dznl::f64>().second == 2);
     static_assert(dznl::compute_precision<dznl::f64>() == 53);
 
 #if defined(__GNUC__) && !defined(__clang__)
-    static_assert(dznl::compute_radix<dznl::f128>() == 2);
+    static_assert(dznl::compute_radix<dznl::f128>().second == 2);
     static_assert(dznl::compute_precision<dznl::f128>() == 113);
-    static_assert(dznl::compute_radix<dznl::d32>() == 10);
+    static_assert(dznl::compute_radix<dznl::d32>().second == 10);
     static_assert(dznl::compute_precision<dznl::d32>() == 7);
-    static_assert(dznl::compute_radix<dznl::d64>() == 10);
+    static_assert(dznl::compute_radix<dznl::d64>().second == 10);
     static_assert(dznl::compute_precision<dznl::d64>() == 16);
-    static_assert(dznl::compute_radix<dznl::d128>() == 10);
+    static_assert(dznl::compute_radix<dznl::d128>().second == 10);
     static_assert(dznl::compute_precision<dznl::d128>() == 34);
 #endif // defined(__GNUC__) && !defined(__clang__)
 
     using namespace boost::multiprecision;
 
     // clang-format off
-    if (dznl::compute_radix<cpp_bin_float_single>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_single>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_single>() != 24) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_double>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_double>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_double>() != 53) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_double_extended>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_double_extended>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_double_extended>() != 64) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_quad>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_quad>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_quad>() != 113) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_oct>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_oct>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_oct>() != 237) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_50>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_50>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_50>() != 168) { return EXIT_FAILURE; }
-    if (dznl::compute_radix<cpp_bin_float_100>() != 2) { return EXIT_FAILURE; }
+    if (dznl::compute_radix<cpp_bin_float_100>().second != 2) { return EXIT_FAILURE; }
     if (dznl::compute_precision<cpp_bin_float_100>() != 334) { return EXIT_FAILURE; }
     // clang-format on
 }
