c51ca96574948ccdf461cfd9c5c7138b3e97af3c
David K. Zhang <dzhang314@gmail.com>
2024-12-13 05:37:30 -0800

    change i64 and u64 to long long for Windows compat

--- dznl/NumericTypes.hpp
+++ dznl/NumericTypes.hpp
@@ -7,11 +7,11 @@ namespace dznl {
 using i8 = signed char;
 using i16 = signed short int;
 using i32 = signed int;
-using i64 = signed long int;
+using i64 = signed long long int;
 using u8 = unsigned char;
 using u16 = unsigned short int;
 using u32 = unsigned int;
-using u64 = unsigned long int;
+using u64 = unsigned long long int;
 using f32 = float;
 using f64 = double;
 
