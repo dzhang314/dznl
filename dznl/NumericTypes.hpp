b7146549f33776f669f698443c8163deda2f11d8
David K. Zhang <dzhang314@gmail.com>
2024-12-21 17:34:11 -0600

    use DZNL_CHAR_BIT in static size assertions

--- dznl/NumericTypes.hpp
+++ dznl/NumericTypes.hpp
@@ -16,16 +16,16 @@ using f32 = float;
 using f64 = double;
 
 
-static_assert(sizeof(i8) == 1);
-static_assert(sizeof(i16) == 2);
-static_assert(sizeof(i32) == 4);
-static_assert(sizeof(i64) == 8);
-static_assert(sizeof(u8) == 1);
-static_assert(sizeof(u16) == 2);
-static_assert(sizeof(u32) == 4);
-static_assert(sizeof(u64) == 8);
-static_assert(sizeof(f32) == 4);
-static_assert(sizeof(f64) == 8);
+static_assert(DZNL_CHAR_BIT * sizeof(i8) == 8);
+static_assert(DZNL_CHAR_BIT * sizeof(i16) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(i32) == 32);
+static_assert(DZNL_CHAR_BIT * sizeof(i64) == 64);
+static_assert(DZNL_CHAR_BIT * sizeof(u8) == 8);
+static_assert(DZNL_CHAR_BIT * sizeof(u16) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(u32) == 32);
+static_assert(DZNL_CHAR_BIT * sizeof(u64) == 64);
+static_assert(DZNL_CHAR_BIT * sizeof(f32) == 32);
+static_assert(DZNL_CHAR_BIT * sizeof(f64) == 64);
 
 
 #ifdef DZNL_REQUEST_I128
@@ -33,7 +33,7 @@ static_assert(sizeof(f64) == 8);
 #error "128-bit integers are not supported on this platform"
 #endif // __SIZEOF_INT128__
 using i128 = __int128_t;
-static_assert(sizeof(i128) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(i128) == 128);
 #endif // DZNL_REQUEST_I128
 
 
@@ -42,13 +42,13 @@ static_assert(sizeof(i128) == 16);
 #error "128-bit integers are not supported on this platform"
 #endif // __SIZEOF_INT128__
 using u128 = __uint128_t;
-static_assert(sizeof(u128) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(u128) == 128);
 #endif // DZNL_REQUEST_U128
 
 
 #ifdef DZNL_REQUEST_F16
 using f16 = _Float16;
-static_assert(sizeof(f16) == 2);
+static_assert(DZNL_CHAR_BIT * sizeof(f16) == 16);
 #endif // DZNL_REQUEST_F16
 
 
@@ -59,7 +59,7 @@ static_assert(sizeof(f16) == 2);
 #error "bfloat16 support requires GCC 13, Clang 17, or later"
 #else
 using bf16 = __bf16;
-static_assert(sizeof(bf16) == 2);
+static_assert(DZNL_CHAR_BIT * sizeof(bf16) == 16);
 #endif
 #endif // DZNL_REQUEST_BF16
 
@@ -69,7 +69,7 @@ static_assert(sizeof(bf16) == 2);
 #error "128-bit floating-point support requires GCC"
 #else
 using f128 = __float128;
-static_assert(sizeof(f128) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(f128) == 128);
 #endif
 #endif // DZNL_REQUEST_F128
 
@@ -79,7 +79,7 @@ static_assert(sizeof(f128) == 16);
 #error "decimal floating-point support requires GCC"
 #else
 using d32 = float [[gnu::mode(SD)]];
-static_assert(sizeof(d32) == 4);
+static_assert(DZNL_CHAR_BIT * sizeof(d32) == 32);
 #endif
 #endif // DZNL_REQUEST_D32
 
@@ -89,7 +89,7 @@ static_assert(sizeof(d32) == 4);
 #error "decimal floating-point support requires GCC"
 #else
 using d64 = float [[gnu::mode(DD)]];
-static_assert(sizeof(d64) == 8);
+static_assert(DZNL_CHAR_BIT * sizeof(d64) == 64);
 #endif
 #endif // DZNL_REQUEST_D64
 
@@ -99,7 +99,7 @@ static_assert(sizeof(d64) == 8);
 #error "decimal floating-point support requires GCC"
 #else
 using d128 = float [[gnu::mode(TD)]];
-static_assert(sizeof(d128) == 16);
+static_assert(DZNL_CHAR_BIT * sizeof(d128) == 128);
 #endif
 #endif // DZNL_REQUEST_D128
 
