f7c17d4d1108b5273861f3ba5d6a5e9b58a25798
David K. Zhang <dzhang314@gmail.com>
2024-12-23 15:40:38 -0800

    suppress MSVC logo printing

--- test_windows.ps1
+++ test_windows.ps1
@@ -1,5 +1,5 @@
 Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell 08c4929b
+Enter-VsDevShell 08c4929b -DevCmdArguments "-arch=x64 -no_logo"
 
 function Compile-And-Run-MSVC {
     param (
@@ -7,7 +7,7 @@ function Compile-And-Run-MSVC {
         [string]$outputFile
     )
 
-    cl /std:c++17 /EHsc `
+    cl /nologo /std:c++17 /EHsc `
         /Wall /wd4514 /wd4577 /wd4820 /wd5045 `
         /external:W0 /external:I "C:\Programs\boost_1_87_0" /I . `
         $sourceFile /Fo:bin\ /Fe:$outputFile
@@ -65,3 +65,7 @@ Compile-And-Run-Clang test\TestFloatingPoint.cpp bin\TestFloatingPointClang.exe
 Compile-And-Run-MSVC test\TestFloatToString.cpp bin\TestFloatToStringMSVC.exe
 Compile-And-Run-GCC test\TestFloatToString.cpp bin\TestFloatToStringGCC.exe
 Compile-And-Run-Clang test\TestFloatToString.cpp bin\TestFloatToStringClang.exe
+
+Compile-And-Run-MSVC test\TestToHexString.cpp bin\TestToHexStringMSVC.exe
+Compile-And-Run-GCC test\TestToHexString.cpp bin\TestToHexStringGCC.exe
+Compile-And-Run-Clang test\TestToHexString.cpp bin\TestToHexStringClang.exe
