0cae20f00419af9eb6508ffad9a29a282080b41a
David K. Zhang <dzhang314@gmail.com>
2025-01-26 23:26:28 -0800

    use SELTZO lemmas in network_verifier

--- scripts/network_verifier.py
+++ scripts/network_verifier.py
@@ -2,7 +2,10 @@
 
 import z3
 
+from se_lemmas import two_sum_se_lemmas
 from setz_lemmas import two_sum_setz_lemmas
+from seltzo_lemmas import two_sum_seltzo_lemmas
+from sys import argv
 from time import perf_counter_ns
 
 
@@ -159,71 +162,94 @@ def two_sum(
     s = FPVariable(solver, sum_name, precision=p, zero_exponent=zx)
     e = FPVariable(solver, err_name, precision=p, zero_exponent=zx)
 
-    lemmas: dict[str, z3.BoolRef] = two_sum_setz_lemmas(
-        x,
-        y,
-        s,
-        e,
-        x.sign_bit,
-        y.sign_bit,
-        s.sign_bit,
-        e.sign_bit,
-        x.exponent,
-        y.exponent,
-        s.exponent,
-        e.exponent,
-        z3.If(x.trailing_bit, z3.IntVal(0), x.num_trailing_bits),
-        z3.If(y.trailing_bit, z3.IntVal(0), y.num_trailing_bits),
-        z3.If(s.trailing_bit, z3.IntVal(0), s.num_trailing_bits),
-        z3.If(e.trailing_bit, z3.IntVal(0), e.num_trailing_bits),
-        lambda v: v.is_zero,
-        lambda v: z3.Not(v.sign_bit),
-        lambda v: v.sign_bit,
-        lambda v, w: v.maybe_equal(w),
-        z3.IntVal(p),
-        z3.IntVal(1),
-        z3.IntVal(2),
-        z3.IntVal(3),
-    )
-
-    # lemmas: dict[str, z3.BoolRef] = two_sum_se_lemmas(
-    #     x,
-    #     y,
-    #     s,
-    #     e,
-    #     x.sign_bit,
-    #     y.sign_bit,
-    #     s.sign_bit,
-    #     e.sign_bit,
-    #     # x.leading_bit,
-    #     # y.leading_bit,
-    #     # s.leading_bit,
-    #     # e.leading_bit,
-    #     # x.trailing_bit,
-    #     # y.trailing_bit,
-    #     # s.trailing_bit,
-    #     # e.trailing_bit,
-    #     x.exponent,
-    #     y.exponent,
-    #     s.exponent,
-    #     e.exponent,
-    #     # x.num_leading_bits,
-    #     # y.num_leading_bits,
-    #     # s.num_leading_bits,
-    #     # e.num_leading_bits,
-    #     # x.num_trailing_bits,
-    #     # y.num_trailing_bits,
-    #     # s.num_trailing_bits,
-    #     # e.num_trailing_bits,
-    #     lambda v: v.is_zero,
-    #     lambda v: z3.Not(v.sign_bit),
-    #     lambda v: v.sign_bit,
-    #     lambda v, w: v.maybe_equal(w),
-    #     z3.IntVal(p),
-    #     z3.IntVal(1),
-    #     z3.IntVal(2),
-    #     # z3.IntVal(3),
-    # )
+    if "--se" in argv:
+        lemmas: dict[str, z3.BoolRef] = two_sum_se_lemmas(
+            x,
+            y,
+            s,
+            e,
+            x.sign_bit,
+            y.sign_bit,
+            s.sign_bit,
+            e.sign_bit,
+            x.exponent,
+            y.exponent,
+            s.exponent,
+            e.exponent,
+            lambda v: v.is_zero,
+            lambda v: z3.Not(v.sign_bit),
+            lambda v: v.sign_bit,
+            lambda v, w: v.maybe_equal(w),
+            z3.IntVal(p),
+            z3.IntVal(1),
+            z3.IntVal(2),
+        )
+    else:
+        lemmas: dict[str, z3.BoolRef] = two_sum_setz_lemmas(
+            x,
+            y,
+            s,
+            e,
+            x.sign_bit,
+            y.sign_bit,
+            s.sign_bit,
+            e.sign_bit,
+            x.exponent,
+            y.exponent,
+            s.exponent,
+            e.exponent,
+            z3.If(x.trailing_bit, z3.IntVal(0), x.num_trailing_bits),
+            z3.If(y.trailing_bit, z3.IntVal(0), y.num_trailing_bits),
+            z3.If(s.trailing_bit, z3.IntVal(0), s.num_trailing_bits),
+            z3.If(e.trailing_bit, z3.IntVal(0), e.num_trailing_bits),
+            lambda v: v.is_zero,
+            lambda v: z3.Not(v.sign_bit),
+            lambda v: v.sign_bit,
+            lambda v, w: v.maybe_equal(w),
+            z3.IntVal(p),
+            z3.IntVal(1),
+            z3.IntVal(2),
+            z3.IntVal(3),
+        )
+        if "--setz" not in argv:
+            lemmas |= two_sum_seltzo_lemmas(
+                x,
+                y,
+                s,
+                e,
+                x.sign_bit,
+                y.sign_bit,
+                s.sign_bit,
+                e.sign_bit,
+                x.leading_bit,
+                y.leading_bit,
+                s.leading_bit,
+                e.leading_bit,
+                x.trailing_bit,
+                y.trailing_bit,
+                s.trailing_bit,
+                e.trailing_bit,
+                x.exponent,
+                y.exponent,
+                s.exponent,
+                e.exponent,
+                x.num_leading_bits,
+                y.num_leading_bits,
+                s.num_leading_bits,
+                e.num_leading_bits,
+                x.num_trailing_bits,
+                y.num_trailing_bits,
+                s.num_trailing_bits,
+                e.num_trailing_bits,
+                lambda v: v.is_zero,
+                lambda v: z3.Not(v.sign_bit),
+                lambda v: v.sign_bit,
+                lambda v, w: v.maybe_equal(w),
+                z3.IntVal(p),
+                z3.IntVal(1),
+                z3.IntVal(2),
+                z3.IntVal(3),
+            )
 
     for claim in lemmas.values():
         solver.add(claim)
@@ -353,7 +379,7 @@ def verify_joldes_2017_algorithm_4(p: int, suffix: str = "") -> None:
     # SE: 2p - 4
     # SETZ: 2p - 3
     prove(solver, a3.is_ulp_nonoverlapping(b3), "A4N" + suffix, variables)
-    prove(solver, c2.is_smaller_than(a3, 2 * p - 3), "A4E" + suffix, variables)
+    prove(solver, c2.is_smaller_than(a3, 2 * p - 2), "A4E" + suffix, variables)
 
 
 def verify_joldes_2017_algorithm_6(p: int, suffix: str = "") -> None:
@@ -423,7 +449,7 @@ def verify_zhang_addition(p: int, suffix: str = "") -> None:
     # SE: 2p - 6
     # SETZ: 2p - 3
     prove(solver, a4.is_ulp_nonoverlapping(b4), "ZAN" + suffix, variables)
-    prove(solver, c4.is_smaller_than(a4, 2 * p - 3), "ZAE" + suffix, variables)
+    prove(solver, c4.is_smaller_than(a4, 2 * p - 2), "ZAE" + suffix, variables)
 
 
 if __name__ == "__main__":
